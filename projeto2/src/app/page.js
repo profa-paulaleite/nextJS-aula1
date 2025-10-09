0716dea84686ad086e7a66b2f9fcc1693505cd1e
profa-paulaleite <paula.leite@mackenzie.br>
2025-10-09 11:38:30 +0000

    codigo criado na aula 9

---
+++ projeto2/src/app/page.js
@@ -0,0 +1,41 @@
+"use client"
+import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [tarefa, setTarefa] = useState("");
+  const [tarefas, setTarefas] = useState([]);
+
+  useEffect(() => {
+    console.log("Número de tarefas:", tarefas.length);
+  }, [tarefas]);
+
+  function adicionarTarefa() {
+    if (tarefa.trim != "") {
+      setTarefas([...tarefas, tarefa]);
+      setTarefa("");
+    }
+  }
+
+  function removerTarefa(index) {
+    const tarefasAtualizadas = tarefas.filter((item, i) => i !== index);
+    setTarefas(tarefasAtualizadas);
+  }
+
+  return (
+    <section>
+      <h1> Lista de Tarefas </h1>
+      <input type="text" value={tarefa} onChange={(trf) => setTarefa(trf.target.value)} placeholder="Digite uma tarefa" />
+      <button onClick={adicionarTarefa}> Adicionar </button>
+
+      <ul>
+        {tarefas.map((trf, index) => (
+          <li key={index}> 
+            {trf} 
+            <button onClick={() => removerTarefa(index)}> Remover </button>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
