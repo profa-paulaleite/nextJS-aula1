edc2a74a0b1e4383e246027f2117d48cd1061b71
profa-paulaleite <paula.leite@mackenzie.br>
2025-10-27 10:58:42 +0000

    configurações padrões

---
+++ atv-get/src/app/layout.js
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Aula GET",
+  description: "Aula sobre o conceito de GET no Next.js",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
